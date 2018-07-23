887f60be7d87497afe4ec0f05c69e52c38b91b49
Ashwanth <ashwanthmuruhathasan@gmail.com>
2018-07-22 17:25:19 -0700

    bubble sort, heap sort

--- src/_00_Sorting_Algorithms/BogoSorter.java
+++ src/_00_Sorting_Algorithms/BogoSorter.java
@@ -3,6 +3,10 @@ package _00_Sorting_Algorithms;
 import java.util.Random;
 
 public class BogoSorter extends Sorter {
+	
+	Random rand = new Random();
+	Random ran = new Random();
+	
 	public BogoSorter() {
 		type = "Bogo";
 	}
@@ -20,6 +24,34 @@ public class BogoSorter extends Sorter {
 	//1. Complete the sort method using the Bogo sort algorithm. 
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-
+		
+		while (!intArraySorted(array)) {
+			
+			int ran1 = rand.nextInt(array.length);
+			int ran2 = ran.nextInt(array.length);
+			
+			int placeholder = array[ran1];
+			int two = array[ran2];
+			
+			array[ran1] = two;
+			array[ran2] = placeholder;
+			
+			display.updateDisplay();
+			
+		}
+		
 	}
+	
+	static boolean intArraySorted(int[] array) {
+		
+		boolean hrmmmnrmmm = true;
+		
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				hrmmmnrmmm = false;
+			}
+		}
+		return hrmmmnrmmm;
+	}
+	
 }
--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -11,6 +11,31 @@ public class BubbleSorter extends Sorter{
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		
+		if (!intArraySorted(array)) {
+			for (int i = 0; i < array.length; i++) {
+				for (int j = i + 1; j < array.length; j++) {
+					if (array[i] > array[j]) {
+						int placeholder = array[j];
+						array[j] = array[i];
+						array[i] = placeholder;
+						display.updateDisplay();
+					}
+				}
+			}
+		}
+		
+	}
+	
+	static boolean intArraySorted(int[] array) {
+		
+		boolean hrmmmnrmmm = true;
+		
+		for (int i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				hrmmmnrmmm = false;
+			}
+		}
+		return hrmmmnrmmm;
 	}
 	
 }
